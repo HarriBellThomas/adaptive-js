6b1caf24e3ba8a2a391ba601f9197b1ea7e05bbe
Louis <louisfharris@gmail.com>
2018-02-07 00:42:02 +0000

    Clean up & fix.

--- src/modules/colourManipulations.js
+++ src/modules/colourManipulations.js
@@ -3,35 +3,32 @@ registerNamespace("uk.org.adaptive.colourManipulations");
 self.isActive = false;
 
 registerNSMethod(self, "changeSaturation", (
-    function (properties) {
+  function (properties) {
 
-        if (!verifyArgs(properties, [["factor", NUMTYPE]]))
-            return false;
+    if (!verifyArgs(properties, [["factor", NUMTYPE]]))
+      return false;
 
-        if (self.isActive)
-            self.remove();
+    if (self.isActive)
+      self.remove();
 
-        self.isActive = true;
+    self.isActive = true;
 
-        forall(IMAGES).do(
-            function (a) {
-                applyToImage(a, function () {
+    forall(IMAGES).do(
+      function (a) {
+        applyToImage(a, function () {
 
-                    if (!self.isActive) return;
-
-                    this.cacheCSSProperties(["filter"]);
-
-                    var value = properties["factor"];
-                    this.style.filter = "saturate("+value+"%)";
-                })
-            });
-    }
+          if (!self.isActive) return;
+          
+          var value = properties["factor"];
+          this.style.filter = "saturate(" + value + "%)";
+        })
+      });
+  }
 ));
 
 registerNSMethod(self, "remove", (
-    function () {
-        self.isActive = false;
-        this.resetCSS();
-        return true;
-    }
+  function () {
+    self.isActive = false;
+    return true;
+  }
 ));
