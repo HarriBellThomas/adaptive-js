78fc96219fd229137ffb594674dbab478d5584aa
Harri Bell-Thomas <harribt@live.co.uk>
2018-02-09 15:57:01 +0000

    Updated the deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,6 @@ cp -r $TRAVIS_BUILD_DIR ../pkg-js && \
 tar -czf travis-deploy-js.tgz ../pkg-js/adaptive-js && \
 rm -rf ../pkg-js && \
 openssl aes-256-cbc -K $encrypted_cbdb4f80c025_key -iv $encrypted_cbdb4f80c025_iv -in deploy.enc -out deploy-hbt-adaptive -d && \
-ls -la && \
 chmod 0700 deploy-hbt-adaptive && \
 ssh-add deploy-hbt-adaptive && \
 scp -i deploy-hbt-adaptive travis-deploy-js.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -5,7 +5,7 @@ cd /var/www && \
 rm -rf js/* && \
 tar zxvf travis-deploy-js.tgz -C ./ && \
 cp -r pkg-js/adaptive-js/src/* js && \
+ls -la js && \
 chown -R deploy:www-data js && \
 chmod -R 775 js && \
 rm -rf pkg-js
-
