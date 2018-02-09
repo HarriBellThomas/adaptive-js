7c97b13ac70e65bb13e90f343f4a2f4cd93744c8
Louis <louisfharris@gmail.com>
2018-02-09 20:20:15 +0000

    Edited background colour function.

--- src/modules/imageColourShifter.js
+++ src/modules/imageColourShifter.js
@@ -80,7 +80,7 @@ registerNSMethod(self, "apply", (
         })
       });
 
-    forall(DIVS,LINKS,SPANS,TABLES).do(
+    forall(DIVS, LINKS, SPANS, TABLES).do(
       function (a) {
 
         if (!self.isActive) return;
@@ -91,34 +91,192 @@ registerNSMethod(self, "apply", (
         bc = a.style.backgroundColor;
         c = a.style.color;
 
+        if (bc.startsWith("rgb")) {
 
-        bColorRGBArray = bc.split("(");
-        bColorRGBArray = bColorRGBArray[1].split(")");
-        bColorRGBArray = bColorRGBArray[1].split(", ");
+          bc = bc.substring(4, bc.length - 1)
+            .replace(/ /g, '')
+            .split(',');
 
-        colorRGBArray = c.split("(");
-        colorRGBArray = colorRGBArray[1].split(")");
-        colorRGBArray = colorRGBArray[1].split(", ");
+          r = bc[0] * matrix.R[0] / 100.0 + bc[1] * matrix.R[1] / 100.0 + bc[2] * matrix.R[2] / 100.0;
+          g = bc[0] * matrix.G[0] / 100.0 + bc[1] * matrix.G[1] / 100.0 + bc[2] * matrix.G[2] / 100.0;
+          b = bc[0] * matrix.B[0] / 100.0 + bc[1] * matrix.B[1] / 100.0 + bc[2] * matrix.B[2] / 100.0;
 
-        bRed = bColorRGBArray[0];
-        bGreen = bColorRGBArray[1];
-        bBlue = bColorRGBArray[2];
+          a.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 
-        red = colorRGBArray[0];
-        green = colorRGBArray[1];
-        blue = colorRGBArray[2];
+        } else {
 
-        bRed = bRed * matrix.R[0] / 100.0 +bGreen * matrix.R[1] / 100.0 + bBlue * matrix.R[2] / 100.0;
-        bGreen = bRed * matrix.G[0] / 100.0 +bGreen * matrix.G[1] / 100.0 + bBlue * matrix.G[2] / 100.0;
-        bBlue = bRed * matrix.B[0] / 100.0 + bGreen * matrix.B[1] / 100.0 + bBlue * matrix.B[2] / 100.0;
+          colourNameToHex = function (colour) {
+            var colours = {
+              "aliceblue": "#f0f8ff",
+              "antiquewhite": "#faebd7",
+              "aqua": "#00ffff",
+              "aquamarine": "#7fffd4",
+              "azure": "#f0ffff",
+              "beige": "#f5f5dc",
+              "bisque": "#ffe4c4",
+              "black": "#000000",
+              "blanchedalmond": "#ffebcd",
+              "blue": "#0000ff",
+              "blueviolet": "#8a2be2",
+              "brown": "#a52a2a",
+              "burlywood": "#deb887",
+              "cadetblue": "#5f9ea0",
+              "chartreuse": "#7fff00",
+              "chocolate": "#d2691e",
+              "coral": "#ff7f50",
+              "cornflowerblue": "#6495ed",
+              "cornsilk": "#fff8dc",
+              "crimson": "#dc143c",
+              "cyan": "#00ffff",
+              "darkblue": "#00008b",
+              "darkcyan": "#008b8b",
+              "darkgoldenrod": "#b8860b",
+              "darkgray": "#a9a9a9",
+              "darkgreen": "#006400",
+              "darkkhaki": "#bdb76b",
+              "darkmagenta": "#8b008b",
+              "darkolivegreen": "#556b2f",
+              "darkorange": "#ff8c00",
+              "darkorchid": "#9932cc",
+              "darkred": "#8b0000",
+              "darksalmon": "#e9967a",
+              "darkseagreen": "#8fbc8f",
+              "darkslateblue": "#483d8b",
+              "darkslategray": "#2f4f4f",
+              "darkturquoise": "#00ced1",
+              "darkviolet": "#9400d3",
+              "deeppink": "#ff1493",
+              "deepskyblue": "#00bfff",
+              "dimgray": "#696969",
+              "dodgerblue": "#1e90ff",
+              "firebrick": "#b22222",
+              "floralwhite": "#fffaf0",
+              "forestgreen": "#228b22",
+              "fuchsia": "#ff00ff",
+              "gainsboro": "#dcdcdc",
+              "ghostwhite": "#f8f8ff",
+              "gold": "#ffd700",
+              "goldenrod": "#daa520",
+              "gray": "#808080",
+              "green": "#008000",
+              "greenyellow": "#adff2f",
+              "honeydew": "#f0fff0",
+              "hotpink": "#ff69b4",
+              "indianred ": "#cd5c5c",
+              "indigo": "#4b0082",
+              "ivory": "#fffff0",
+              "khaki": "#f0e68c",
+              "lavender": "#e6e6fa",
+              "lavenderblush": "#fff0f5",
+              "lawngreen": "#7cfc00",
+              "lemonchiffon": "#fffacd",
+              "lightblue": "#add8e6",
+              "lightcoral": "#f08080",
+              "lightcyan": "#e0ffff",
+              "lightgoldenrodyellow": "#fafad2",
+              "lightgrey": "#d3d3d3",
+              "lightgreen": "#90ee90",
+              "lightpink": "#ffb6c1",
+              "lightsalmon": "#ffa07a",
+              "lightseagreen": "#20b2aa",
+              "lightskyblue": "#87cefa",
+              "lightslategray": "#778899",
+              "lightsteelblue": "#b0c4de",
+              "lightyellow": "#ffffe0",
+              "lime": "#00ff00",
+              "limegreen": "#32cd32",
+              "linen": "#faf0e6",
+              "magenta": "#ff00ff",
+              "maroon": "#800000",
+              "mediumaquamarine": "#66cdaa",
+              "mediumblue": "#0000cd",
+              "mediumorchid": "#ba55d3",
+              "mediumpurple": "#9370d8",
+              "mediumseagreen": "#3cb371",
+              "mediumslateblue": "#7b68ee",
+              "mediumspringgreen": "#00fa9a",
+              "mediumturquoise": "#48d1cc",
+              "mediumvioletred": "#c71585",
+              "midnightblue": "#191970",
+              "mintcream": "#f5fffa",
+              "mistyrose": "#ffe4e1",
+              "moccasin": "#ffe4b5",
+              "navajowhite": "#ffdead",
+              "navy": "#000080",
+              "oldlace": "#fdf5e6",
+              "olive": "#808000",
+              "olivedrab": "#6b8e23",
+              "orange": "#ffa500",
+              "orangered": "#ff4500",
+              "orchid": "#da70d6",
+              "palegoldenrod": "#eee8aa",
+              "palegreen": "#98fb98",
+              "paleturquoise": "#afeeee",
+              "palevioletred": "#d87093",
+              "papayawhip": "#ffefd5",
+              "peachpuff": "#ffdab9",
+              "peru": "#cd853f",
+              "pink": "#ffc0cb",
+              "plum": "#dda0dd",
+              "powderblue": "#b0e0e6",
+              "purple": "#800080",
+              "rebeccapurple": "#663399",
+              "red": "#ff0000",
+              "rosybrown": "#bc8f8f",
+              "royalblue": "#4169e1",
+              "saddlebrown": "#8b4513",
+              "salmon": "#fa8072",
+              "sandybrown": "#f4a460",
+              "seagreen": "#2e8b57",
+              "seashell": "#fff5ee",
+              "sienna": "#a0522d",
+              "silver": "#c0c0c0",
+              "skyblue": "#87ceeb",
+              "slateblue": "#6a5acd",
+              "slategray": "#708090",
+              "snow": "#fffafa",
+              "springgreen": "#00ff7f",
+              "steelblue": "#4682b4",
+              "tan": "#d2b48c",
+              "teal": "#008080",
+              "thistle": "#d8bfd8",
+              "tomato": "#ff6347",
+              "turquoise": "#40e0d0",
+              "violet": "#ee82ee",
+              "wheat": "#f5deb3",
+              "white": "#ffffff",
+              "whitesmoke": "#f5f5f5",
+              "yellow": "#ffff00",
+              "yellowgreen": "#9acd32"
+            };
 
-        red = red * matrix.R[0] / 100.0 + green * matrix.R[1] / 100.0 + blue * matrix.R[2] / 100.0;
-        green = red * matrix.G[0] / 100.0 + green * matrix.G[1] / 100.0 + blue * matrix.G[2] / 100.0;
-        blue = red * matrix.B[0] / 100.0 + green * matrix.B[1] / 100.0 + blue * matrix.B[2] / 100.0;
+            if (typeof colours[colour.toLowerCase()] != 'undefined')
+              return colours[colour.toLowerCase()];
 
-        a.style.backgroundColor = "rgb("+bRed+","+bGreen+","+bBlue+")";
-        a.style.color = "rgb("+red+","+green+","+blue+")";
+            return false;
+          };
 
+          if (!bc.startsWith("#")) {
+            bc = colourNameToHex(bc);
+          }
+
+          hexToRgb = function (hex) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+              r: parseInt(result[1], 16),
+              g: parseInt(result[2], 16),
+              b: parseInt(result[3], 16)
+            } : null;
+          };
+
+          rgb = hexToRgb(bc);
+
+          r = bc[0] * matrix.R[0] / 100.0 + bc[1] * matrix.R[1] / 100.0 + bc[2] * matrix.R[2] / 100.0;
+          g = bc[0] * matrix.G[0] / 100.0 + bc[1] * matrix.G[1] / 100.0 + bc[2] * matrix.G[2] / 100.0;
+          b = bc[0] * matrix.B[0] / 100.0 + bc[1] * matrix.B[1] / 100.0 + bc[2] * matrix.B[2] / 100.0;
+
+          a.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"
+        }
       }
     )
   }
