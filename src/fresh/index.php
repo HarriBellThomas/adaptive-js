82cda6f75322a9284aad02767c443f201c38dc2d
carlos <cp614@cam.ac.uk>
2018-02-10 11:45:33 +0000

    More debugging

--- src/fresh/index.php
+++ src/fresh/index.php
@@ -14,7 +14,9 @@
     if( file_exists("../modules/".$modules[$i].".js") ){
 
         $o1 = compileFile("../modules/".$modules[$i].".js", $noOfStrings);
-        $compiled .= "\n\n/* File: ".$modules[$i]."*/ \n\n".$o1[0];
+        //$compiled .= "\n\n/* File: ".$modules[$i]."*/ \n\n".$o1[0];
+
+        $compiled .= "\n\n/* File: ".$modules[$i]."*/ \n\ntry{".$o1[0]."}catch(e){if (debug!=undefined) debug(e.message+' in file '+'"+$modules[i]+"');}";
         $stringsF .= $o1[1];
         $noOfStrings = $o1[2];
         $testsF .= $o1[3];
