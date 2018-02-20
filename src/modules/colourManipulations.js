30537031745293bb21d0f1512bf55510c728db43
Louis <louisfharris@gmail.com>
2018-02-20 12:27:09 +0000

    Fix.

--- src/modules/colourManipulations.js
+++ src/modules/colourManipulations.js
@@ -85,78 +85,6 @@ registerNSMethod(self, "changeContrast", (
     if (self.isActive)
       self.remove();
 
-    limit = function(v) {
-      if (v<0) return 0;
-      if (v>1) return 1;
-      return v;
-    };
-
-    value = properties["factor"];
-
-    forall(VISUALS).do(
-      function (a) {
-        applyToImage(a, function (xy,rgba) {
-
-          if (!self.isActive) return;
-
-          hsl = rgbToHsl(rgba.r,rgba.g,rgba.b);
-          hsl[1] = limit(hsl[1]*value);
-
-          rgb = hslToRgb(hsl[0],hsl[1],hsl[2]);
-          return {
-            r: Math.round(rgb[0]),
-            g: Math.round(rgb[1]),
-            b: Math.round(rgb[2]),
-            a: rgba.a
-          }
-        })
-      });
-
-    forall().do(
-      function (a) {
-        if (!self.isActive) return;
-
-        bc = rgbaValue(extractColour(a, "backgroundColor"));
-        c = rgbaValue(extractColour(a, "color"));
-        boc = rgbaValue(extractColour(a, "border-color"));
-
-        bchsl = rgbToHsl(bc.r,bc.g,bc.b);
-        chsl = rgbToHsl(c.r,c.g,c.b);
-        bochsl = rgbToHsl(boc.r,boc.g,boc.b);
-
-        bchsl[1] = limit(bchsl[1]*value);
-        chsl[1] = limit(chsl[1]*value);
-        bochsl[1] = limit(bochsl[1]*value);
-
-        rgb1 = hslToRgb(bchsl[0],bchsl[1],bchsl[2]);
-        rgb2 = hslToRgb(chsl[0],chsl[1],chsl[2]);
-        rgb3 = hslToRgb(bochsl[0],bochsl[1],bochsl[2]);
-
-        try {
-          a.cacheCSSProperties(["background-color"]);
-          a.cacheCSSProperties(["color"]);
-          a.cacheCSSProperties(["border-color"]);
-
-        } catch (e) {
-          /* some elements do not work with cacheCSSProperties */
-        }
-        a.style.backgroundColor = "rgba("+Math.round(rgb1[0])+","+Math.round(rgb1[1])+","+Math.round(rgb1[2])+","+bc.a+")";
-        a.style.color = "rgba("+Math.round(rgb2[0])+","+Math.round(rgb2[1])+","+Math.round(rgb2[2])+","+c.a+")";
-        a.style.borderColor = "rgba("+Math.round(rgb3[0])+","+Math.round(rgb3[1])+","+Math.round(rgb3[2])+","+boc.a+")";
-      }
-    )
-  }
-));
-
-registerNSMethod(self, "changeBrightness", (
-  function (properties) {
-
-    if (!verifyArgs(properties, [["factor", NUMTYPE]]))
-      return false;
-
-    if (self.isActive)
-      self.remove();
-
     self.isActive = true;
 
     limit = function(v) {
@@ -217,6 +145,76 @@ registerNSMethod(self, "changeBrightness", (
         a.style.backgroundColor = "rgba("+Math.round(rgb1[0])+","+Math.round(rgb1[1])+","+Math.round(rgb1[2])+","+bc.a+")";
         a.style.color = "rgba("+Math.round(rgb2[0])+","+Math.round(rgb2[1])+","+Math.round(rgb2[2])+","+c.a+")";
         a.style.borderColor = "rgba("+Math.round(rgb3[0])+","+Math.round(rgb3[1])+","+Math.round(rgb3[2])+","+boc.a+")";
+      }
+    )
+  }
+));
+
+registerNSMethod(self, "changeBrightness", (
+  function (properties) {
+
+    if (!verifyArgs(properties, [["factor", NUMTYPE]]))
+      return false;
+
+    if (self.isActive)
+      self.remove();
+
+    self.isActive = true;
+
+    limit = function(v) {
+      if (v<0) return 0;
+      if (v>1) return 1;
+      return v;
+    };
+
+    value = properties["factor"];
+
+    forall(VISUALS).do(
+      function (a) {
+        applyToImage(a, function (xy,rgba) {
+
+          if (!self.isActive) return;
+
+          return {
+            r: rgba.r + Math.round(value),
+            g: rgba.g + Math.round(value),
+            b: rgba.b + Math.round(value),
+            a: rgba.a
+          }
+        })
+      });
+
+    forall().do(
+      function (a) {
+        if (!self.isActive) return;
+
+        bc = rgbaValue(extractColour(a, "backgroundColor"));
+        c = rgbaValue(extractColour(a, "color"));
+        boc = rgbaValue(extractColour(a, "border-color"));
+
+        bc.r = bc.r + Math.round(value);
+        bc.g = bc.g + Math.round(value);
+        bc.b = bc.b + Math.round(value);
+
+        c.r = c.r + Math.round(value);
+        c.g = c.g + Math.round(value);
+        c.b = c.b + Math.round(value);
+
+        boc.r = boc.r + Math.round(value);
+        boc.g = boc.g + Math.round(value);
+        boc.b = boc.b + Math.round(value);
+
+        try {
+          a.cacheCSSProperties(["background-color"]);
+          a.cacheCSSProperties(["color"]);
+          a.cacheCSSProperties(["border-color"]);
+
+        } catch (e) {
+          /* some elements do not work with cacheCSSProperties */
+        }
+        a.style.backgroundColor = "rgba("+bc.r+","+bc.g+","+bc.b+","+bc.a+")";
+        a.style.color = "rgba("+c.r+","+c.g+","+c.b+","+c.a+")";
+        a.style.borderColor = "rgba("+boc.r+","+boc.g+","+boc.b+","+boc.a+")";
       }
     )
   }
