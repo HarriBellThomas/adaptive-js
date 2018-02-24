dc131f20a4bf80e15652590cf3705ff53439d0bf
Louis <louisfharris@gmail.com>
2018-02-24 17:41:03 +0000

    remove function.

--- src/modules/speedBar.js
+++ src/modules/speedBar.js
@@ -81,10 +81,8 @@ registerNSMethod(self, "apply",(
 registerNSMethod(self, "remove",(
   function(){
     self.isActive = false;
-    forall().do(function(a) {
-      if (a.className.toString().indexOf("slider")) {
-        if (area.parentNode) area.parentNode.removeChild(area);
-      }
+    forall().where(a=>a.className.toString().indexOf("slider") && area!=null).do(function(a) {
+      if (area.parentNode) area.parentNode.removeChild(area);
     });
     return true;
   }
