fec91f06cb49f7113c507de07575e63f58543de7
Louis <louisfharris@gmail.com>
2018-02-26 15:43:50 +0000

    Improvement tweaks.

--- src/modules/darkMode.js
+++ src/modules/darkMode.js
@@ -33,7 +33,7 @@ registerNSMethod(self, "apply",(
           } catch (e) {
             /* some elements do not work with cacheCSSProperties */
           }
-          alpha = rgbaValue(extractColour(a, backgroundColor)).a;
+          alpha = rgbaValue(extractColour(a, "backgroundColor")).a;
           a.style.color = "white";
           a.style.backgroundColor = "rgba(25,25,25,"+alpha+")";
         }
