7866e71c48c811bfda970651d1642bf2b87a6649
carlos <cp614@cam.ac.uk>
2018-02-27 16:22:39 +0000

    Supports TTS

--- src/modules/paragraphReader.js
+++ src/modules/paragraphReader.js
@@ -13,6 +13,11 @@ registerNamespace("uk.org.adaptive.paragraphReader");
 
 self.activeElement = false;
 self.isActive = false;
+self.activePanelCover = false;
+self.activePanel = false;
+self.wordReadIndex = 0;
+self.reading = false;
+self.words = false;
 
 /* Now, we can define the member method "apply", which
     takes an object, containing the required properties.
@@ -38,14 +43,29 @@ registerNSMethod(self, "apply",(
     self.isActive = true;
 
     forall(PARAGRAPHS).do(a=>{
+      a.style["-khtml-user-select"] = "none";
+      a.style["-webkit-user-select"] = "none";
+      a.style["-moz-user-select"] = "none";
+      a.style["-ms-user-select"] = "none";
+      a.style["-webkit-touch-callout"] = "none";
+      a.style["user-select"] = "none";
+
+
       a.decorateMouseOver(function(){ if (!hasKeyDown(16)) return;
       this.resetCSS();
       differentto(this).where(a=> a.resetCSS != undefined).
       do(c=>{c.cacheCSSProperties(["opacity"]); c.style.opacity = 0.4})})});
 
+
+    forall(PARAGRAPHS).do(a=>{
+      a.decorateMouseUp(function(){ if (!hasKeyDown(16)) return;
+        self.initDisplayForegroundPanel(this.innerText);
+      })});
+
     doOnKeyUp(16, function(){
       self.removeImmediateEffect();
     });
+
 }));
 
 /* Now we define the method 'remove' which removes the effect
@@ -59,6 +79,149 @@ registerNSMethod(self, "remove",(
   function(){
   }
 ));
+
+registerNSMethod(self, "disposeDisplayForegroundPanel", function(){
+  if (self.activePanel === false) return;
+  self.activePanel.innerHTML = self.activePanel.decoyText;
+  var pos = [self.activePanel.dOffset,window.innerHeight];
+  const diff = pos[1]-pos[0];
+
+  window.speechSynthesis.cancel();
+/*
+  (function(){var l=function(){
+    setTimeout(function(){
+      pos[0]+=20;
+      if (pos[0] < pos[1]){
+        self.activePanel.style.top = pos[0]+"px";
+        self.activePanelCover.style.backgroundColor = "rgba(255,255,255,"+(0.8*(pos[1]-pos[0])/diff)+")";
+        l();
+      }else{
+        self.activePanelCover.outerHTML = "";
+        self.activePanelCover = false;
+        self.activePanel = false;
+      }
+    }, 7)
+  }
+  l();})();
+*/
+    self.activePanel.style.top = pos[1]+"px";
+    setTimeout(function(){
+      self.activePanelCover.outerHTML = "";
+      self.activePanelCover = false;
+      self.activePanel = false;
+    }, 500)
+
+});
+
+(<
+  debug("This is a debug message.");
+  fail("Implementation not finished");
+  require(("Hello").length > 10);
+>)
+
+registerNSMethod(self, "initDisplayForegroundPanel", function(txt){
+  var text = txt.replace(/\ \[[0-9]*\]/g, "");
+
+  if (self.activePanel !== false) return;
+  self.removeImmediateEffect();
+  const foregroundCover = document.createElement("div");
+  foregroundCover.style.position = "fixed";
+  foregroundCover.style.width = "100%";
+  foregroundCover.style.height = "100%";
+  foregroundCover.style.top = "0px";
+  foregroundCover.style.left = "0px";
+  foregroundCover.style.backgroundColor = "rgba(255,255,255,0.8)";
+  foregroundCover.style.overflow = "hidden";
+  foregroundCover.style.backdropFilter = "blur(4px)";
+  foregroundCover.style.webkitBackdropFilter = "blur(4px)";
+  foregroundCover.onmouseup = function(){
+    self.disposeDisplayForegroundPanel();
+  }
+
+
+  const foregroundPanel = document.createElement("div");
+  foregroundPanel.style.width = "600px";
+  foregroundPanel.style.position = "absolute";
+  foregroundPanel.style.backgroundColor = "white";
+  foregroundPanel.style.borderRadius = "10px";
+  foregroundPanel.style.border = "1px solid black";
+  foregroundPanel.style.fontFamily = "Times New Roman";
+  foregroundPanel.style.fontSize = "48px";
+  foregroundPanel.style.color = "black";
+  foregroundPanel.style.padding = "40px 40px 40px 40px";
+  foregroundPanel.style.textAlign = "center";
+  foregroundPanel.style.lineHeight = "54px";
+  foregroundPanel.style.lineHeight = "54px";
+
+
+
+  foregroundPanel.innerHTML = text;
+  foregroundPanel.decoyText = text;
+
+
+  foregroundCover.appendChild(foregroundPanel);
+  document.body.appendChild(foregroundCover);
+
+  self.activePanelCover = foregroundCover;
+  self.activePanel = foregroundPanel;
+
+  var foregroundOutput = text.replace(/\S+/g, a=>"<span>"+a+"</span>");
+  foregroundPanel.style.top = window.innerHeight + "px";
+  foregroundPanel.style.left = ((window.innerWidth-foregroundPanel.clientWidth)/2) + "px";
+  var aimTop = 0;
+  if (foregroundPanel.clientHeight > 0.8*window.innerHeight){
+    aimTop = 0.1*window.innerHeight;
+    foregroundPanel.innerHTML = foregroundPanel.innerHTML.substring(0, 700);
+    foregroundPanel.decoyText = foregroundPanel.innerHTML.substring(0, 700);
+  }else{
+    aimTop = ((window.innerHeight-foregroundPanel.clientHeight)/2);
+  }
+  var pos = [window.innerHeight, aimTop];
+  self.activePanel.dOffset = pos[1];
+
+
+  foregroundPanel.style["-webkit-transition"] = "top 0.5s";
+  foregroundPanel.style.transition = "top 0.5s";
+  foregroundPanel.style.top = pos[1] + "px";
+  foregroundPanel.innerHTML = foregroundOutput;
+
+/*
+  (function(){var l=function(){
+    setTimeout(function(){
+      pos[0]-=20;
+      if (pos[0] > pos[1]){
+        foregroundPanel.style.top = pos[0]+"px";
+        l();
+      }else{
+        foregroundPanel.style.top = pos[1] + "px";
+        foregroundPanel.innerHTML = foregroundOutput;
+      }
+    }, 7)
+  }
+  l();})();
+*/
+  self.wordReadIndex = 0;
+  self.reading = new SpeechSynthesisUtterance(text);
+  self.reading.rate = 4;
+  window.speechSynthesis.speak(self.reading);
+  self.reading.onboundary = self.onWordBoundary;
+  self.words = self.activePanel.getElementsByTagName("span");
+  self.words[self.wordReadIndex].style.backgroundColor = "yellow";
+});
+
+
+registerNSMethod(self, "onWordBoundary", function(text){
+  self.wordReadIndex++;
+  self.words[self.wordReadIndex].style.backgroundColor = "yellow";
+  self.words[self.wordReadIndex-1].style.backgroundColor = "";
+
+  if (self.words[self.wordReadIndex].offsetTop+self.activePanel.dOffset > (window.innerHeight/2)){
+    self.activePanel.dOffset -= self.words[self.wordReadIndex].offsetHeight;
+    self.activePanel.style.top = self.activePanel.dOffset + "px";
+  }
+
+});
+
 
 /* We can now include this module in page by adding
     "linkHighlighter" to the list of modules in the URL and then
