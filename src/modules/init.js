300cce7e942ec5af80fe535d399b813037aed15e
Harri Bell-Thomas <harribt@live.co.uk>
2018-02-27 13:10:08 +0000

    First test in init file

--- src/modules/init.js
+++ src/modules/init.js
@@ -213,3 +213,48 @@ registerNSMethod(uk.org.adaptive, "init", (
 
 /* Shall we begin? */
 uk.org.adaptive.init({id:""});
+
+
+
+
+(<
+
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
+
+    /* Cookies Testing */
+
+    var setCookie = ((cname, cvalue, exdays) => {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    });
+
+    var getCookie = ((cname) => {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    });
+
+    var cookiesRandomGuid = guid();
+    setCookie("ADAPTIVE_test", cookiesRandomGuid, 365);
+    require(getCookie("ADAPTIVE_test") == cookiesRandomGuid)
+
+    /* End Cookies Testing */
+
+>)
