ffa1e248ba8978e0b1a6053947f0e4a76d539298
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-02 19:08:47 +0000

    Added demo override

--- src/modules/init.js
+++ src/modules/init.js
@@ -6,13 +6,6 @@ self.data = null; /* To be initialised by JSON */
 registerNSMethod(uk.org.adaptive, "init", (
     function(properties) {
 
-        var requireAuth = true;
-        var hasAuth = false;
-        var userMode = false;
-        var loginRoute = "https://adaptive.org.uk/api/login/#";
-        var userJSONRoute = "https://adaptive.org.uk/api/default/";
-        var styleJSONRoute = "https://html.adaptive.org.uk/json/example.json#";
-
         var setCookie = ((cname, cvalue, exdays) => {
             var d = new Date();
             d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -90,123 +83,137 @@ registerNSMethod(uk.org.adaptive, "init", (
         }
 
 
+        var requireAuth = true;
+        var hasAuth = false;
+        var userMode = false;
+        var loginRoute = "https://adaptive.org.uk/api/login/#";
+        var userJSONRoute = "https://adaptive.org.uk/api/default/";
+        var styleJSONRoute = "https://html.adaptive.org.uk/json/example.json#";
 
-        var userID = getCookie("ADAPTIVE_A");
-        var styleID = getCookie("ADAPTIVE_B");
 
-        if(window.location.hash) {
-            // Check for auth return
-            try {
-                var hash = window.atob(window.location.hash.substr(1, window.location.hash.length - 1));
-                var data = JSON.parse(hash);
-                console.log(data);
+        /* Demo Mode Bypass */
+        var url = new URL(window.location.href);
+        var demoMode = url.searchParams.get("adaptive_demo");
+        if(demoMode != null) retrieveJSON(userJSONRoute + styleID);
+        /* End Demo Mode Bypass */
 
-                if("user_id" in data) {
-                    setCookie("ADAPTIVE_A", data["user_id"], 365);
-                    userID = data["user_id"];
+        else {
+            var userID = getCookie("ADAPTIVE_A");
+            var styleID = getCookie("ADAPTIVE_B");
+
+            if(window.location.hash) {
+                // Check for auth return
+                try {
+                    var hash = window.atob(window.location.hash.substr(1, window.location.hash.length - 1));
+                    var data = JSON.parse(hash);
+                    console.log(data);
+
+                    if("user_id" in data) {
+                        setCookie("ADAPTIVE_A", data["user_id"], 365);
+                        userID = data["user_id"];
+                    }
+                    if("style_id" in data) {
+                        setCookie("ADAPTIVE_B", data["style_id"], 365);
+                        styleID = data["style_id"];
+                    }
+
+                } catch(e) {
+                    // something failed
+                    console.log("Failed to parse hash.");
+
+                    // if you want to be specific and only catch the error which means
+                    // the base 64 was invalid, then check for 'e.code === 5'.
+                    // (because 'DOMException.INVALID_CHARACTER_ERR === 5')
                 }
-                if("style_id" in data) {
-                    setCookie("ADAPTIVE_B", data["style_id"], 365);
-                    styleID = data["style_id"];
-                }
-
-            } catch(e) {
-                // something failed
-                console.log("Failed to parse hash.");
-
-                // if you want to be specific and only catch the error which means
-                // the base 64 was invalid, then check for 'e.code === 5'.
-                // (because 'DOMException.INVALID_CHARACTER_ERR === 5')
             }
-        }
 
-        if (verifyArgs(properties, [["id", STRINGTYPE]]) && properties["id"] != "") {
-            requireAuth = false;
-            setCookie("ADAPTIVE_B", properties["id"], 365);
-        } else if (styleID != "") {
-            requireAuth = false;
-            userMode = false;
-        } else if (userID != "") {
-            requireAuth = false;
-            userMode = true;
-        } else {
-            requireAuth = true;
-        }
+            if (verifyArgs(properties, [["id", STRINGTYPE]]) && properties["id"] != "") {
+                requireAuth = false;
+                setCookie("ADAPTIVE_B", properties["id"], 365);
+            } else if (styleID != "") {
+                requireAuth = false;
+                userMode = false;
+            } else if (userID != "") {
+                requireAuth = false;
+                userMode = true;
+            } else {
+                requireAuth = true;
+            }
 
 
-        var style = document.createElement("style");
-        style.innerHTML = "div#adaptive-bar{position:fixed;z-index:999999;bottom:0;right:0;height:40px;padding:0;margin:0;background-color:#fff;font-family:Helvetica,system;font-weight:400;font-size:13px;line-height:40px;vertical-align:middle;border-color:#000;border-width:1px 0 0 1px;border-style:solid;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}div#adaptive-bar img{max-height:16px;padding:12px;float:right;box-sizing:content-box}div#adaptive-bar #status{padding:12px;float:right;max-height:16px;font-family:monospace;font-size:13px;line-height:16px;font-weight:1000;background-color:#fff;box-sizing:content-box}div#adaptive-bar #status:hover{opacity:.7}div#adaptive-bar #status.enabled{background-color:green;color:#fff;cursor:pointer}div#adaptive-bar #status.disabled{background-color:#e20000;color:#fff;cursor:pointer}div#adaptive-bar #status.login{background-color:#e20000;color:#fff;cursor:pointer}";
-        document.body.appendChild(style);
+            var style = document.createElement("style");
+            style.innerHTML = "div#adaptive-bar{position:fixed;z-index:999999;bottom:0;right:0;height:40px;padding:0;margin:0;background-color:#fff;font-family:Helvetica,system;font-weight:400;font-size:13px;line-height:40px;vertical-align:middle;border-color:#000;border-width:1px 0 0 1px;border-style:solid;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}div#adaptive-bar img{max-height:16px;padding:12px;float:right;box-sizing:content-box}div#adaptive-bar #status{padding:12px;float:right;max-height:16px;font-family:monospace;font-size:13px;line-height:16px;font-weight:1000;background-color:#fff;box-sizing:content-box}div#adaptive-bar #status:hover{opacity:.7}div#adaptive-bar #status.enabled{background-color:green;color:#fff;cursor:pointer}div#adaptive-bar #status.disabled{background-color:#e20000;color:#fff;cursor:pointer}div#adaptive-bar #status.login{background-color:#e20000;color:#fff;cursor:pointer}";
+            document.body.appendChild(style);
 
-        var adaptiveBar = document.createElement("div");
-        adaptiveBar.setAttribute("data-adaptive", "ignore");
-        adaptiveBar.id = "adaptive-bar";
+            var adaptiveBar = document.createElement("div");
+            adaptiveBar.setAttribute("data-adaptive", "ignore");
+            adaptiveBar.id = "adaptive-bar";
 
-        var imageLink = document.createElement("a");
-        imageLink.href = "https://adaptive.org.uk/";
+            var imageLink = document.createElement("a");
+            imageLink.href = "https://adaptive.org.uk/";
 
-        var image = document.createElement("img");
-        image.src = "https://adaptive.org.uk/images/logo-colourful.png";
+            var image = document.createElement("img");
+            image.src = "https://adaptive.org.uk/images/logo-colourful.png";
 
-        imageLink.appendChild(image);
+            imageLink.appendChild(image);
 
-        var status = document.createElement("span");
-        status.id = "status";
+            var status = document.createElement("span");
+            status.id = "status";
 
-        adaptiveBar.appendChild(imageLink);
-        adaptiveBar.appendChild(status);
+            adaptiveBar.appendChild(imageLink);
+            adaptiveBar.appendChild(status);
 
-        document.body.appendChild(adaptiveBar);
+            document.body.appendChild(adaptiveBar);
 
-        status.addEventListener("click", function(event){
-            event.preventDefault();
-            if(status.className == "disabled") {
-                var lastIndex = window.location.href.indexOf('#');
-                if(lastIndex > -1) {
-                    var url = window.location.href.substr(0, lastIndex);
-                    var hash = window.location.href.substr(lastIndex + 1, window.location.href.length - window.location.hash.length - 1);
+            status.addEventListener("click", function(event){
+                event.preventDefault();
+                if(status.className == "disabled") {
+                    var lastIndex = window.location.href.indexOf('#');
+                    if(lastIndex > -1) {
+                        var url = window.location.href.substr(0, lastIndex);
+                        var hash = window.location.href.substr(lastIndex + 1, window.location.href.length - window.location.hash.length - 1);
+                    }
+                    else {
+                        var url = window.location.href;
+                        var hash = "";
+                    }
+                    var pageData = JSON.stringify({
+                        redirect_url: url,
+                        hash: hash,
+                        time: Date.now(),
+                        hostname: window.location.hostname
+                    });
+                    window.location.replace(loginRoute + window.btoa(pageData));
                 }
+
                 else {
-                    var url = window.location.href;
-                    var hash = "";
+                    removeStyles();
+                    status.className = "disabled";
+                    status.innerHTML = "Disabled";
                 }
-                var pageData = JSON.stringify({
-                    redirect_url: url,
-                    hash: hash,
-                    time: Date.now(),
-                    hostname: window.location.hostname
-                });
-                window.location.replace(loginRoute + window.btoa(pageData));
-            }
+            });
 
-            else {
-                removeStyles();
+            if(requireAuth) {
                 status.className = "disabled";
                 status.innerHTML = "Disabled";
+            } else {
+                status.className = "enabled";
+                status.innerHTML = "Enabled";
+
+                if(userMode) {
+                    // get style from user default style url
+                    retrieveJSON(userJSONRoute + userID);
+                }
+
+                else {
+                    // get style from style route
+                    retrieveJSON(userJSONRoute + styleID);
+                }
+
+                /* Initialise from Style JSON */
+                applyStyles();
             }
-        });
-
-        if(requireAuth) {
-            status.className = "disabled";
-            status.innerHTML = "Disabled";
-        } else {
-            status.className = "enabled";
-            status.innerHTML = "Enabled";
-
-            if(userMode) {
-                // get style from user default style url
-                retrieveJSON(userJSONRoute + userID);
-            }
-
-            else {
-                // get style from style route
-                retrieveJSON(userJSONRoute + styleID);
-            }
-
-            /* Initialise from Style JSON */
-            applyStyles();
         }
-
     }
 ));
 
