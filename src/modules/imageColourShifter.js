e1acc3b62e8ad6ace29e0ddd384b4ad46cebc51e
louis <louisfharris@gmail.com>
2018-03-02 22:09:42 +0000

    VideoTools test.

--- src/modules/imageColourShifter.js
+++ src/modules/imageColourShifter.js
@@ -110,7 +110,7 @@ registerNSMethod(self, "apply", (
         })
       });
 
-    videoTools.apply((xy, rgba)=> {return {r:rgba.r, g:0, b:rgba.g, a:255};});
+    uk.org.adaptive.videoTools.apply((xy, rgba)=> {return {r:rgba.r, g:0, b:rgba.g, a:255};});
   }));
 
 /*Colorspace transformation matrices*/
