3d508231bfec97ba1502388415ffc49a6bee2479
carlos <cp614@cam.ac.uk>
2018-03-03 23:21:12 +0000

    Magnifier update

--- src/modules/magnifier.js
+++ src/modules/magnifier.js
@@ -12,25 +12,25 @@ var mouseY = 0;
 registerNSMethod(self, "apply", function(properties) {
    if (!verifyArgs(properties, [["magnifierSize", NUMTYPE], ["zoom", NUMTYPE]])) return false;
    if (self.isActive) self.remove();
-   
+
    self.isActive = true;
    magnifierSize = properties["magnifierSize"];
    zoom = properties["zoom"];
-   
+
    doOnMouseMove(function(x, y) {
       mouseX = x;
       mouseY = y;
-      
+
       // If we don't have the screenshot yet then don't do anything
       if (typeof magnifyingGlass == "undefined" || !self.isActive) return;
-      
+
       if (isMagnifierOn) updatePosition();
    });
 
    doOnKeyDown(17, function(e) {
       // If we don't have the screenshot yet then don't do anything
       if (typeof magnifyingGlass == "undefined" || !self.isActive) return;
-      
+
       if (!isMagnifierOn) {
          document.body.appendChild(magnifyingGlass);
          isMagnifierOn = true;
@@ -41,16 +41,16 @@ registerNSMethod(self, "apply", function(properties) {
    doOnKeyUp(17, function(e) {
       // If we don't have the screenshot yet then don't do anything
       if (typeof magnifyingGlass == "undefined" || !self.isActive) return;
-      
+
       if (isMagnifierOn) {
          magnifyingGlass.parentNode.removeChild(magnifyingGlass);
          isMagnifierOn = false;
       }
    });
-   
+
    // First, make all cross-domain images not cross domain
    //forall(VISUALS).do(function(a) { applyToImage(a, SOFTIDENTITY); });
-   
+
    // Take screenshot of page
    console.log("Loading screenshot library");
    var script = document.createElement("script");
@@ -58,14 +58,14 @@ registerNSMethod(self, "apply", function(properties) {
    script.async = true;
    script.onload = function() {
       console.log("Taking screenshot");
-      html2canvas(document.body, { scale: zoom, logging: true }).then(function(c) {
+      html2canvas(document.body, { proxy:"https://js.adaptive.org.uk/helpers/canvas.php", scale: zoom, logging: true }).then(function(c) {
          magnifyingGlass = document.createElement("div");
          magnifyingGlass.style.border = "3px solid #000";
          magnifyingGlass.style.borderRadius = "50%";
          magnifyingGlass.style.cursor = "none";
          magnifyingGlass.style.backgroundImage = "url(\"" + c.toDataURL("image/png") + "\")";
          magnifyingGlass.style.backgroundRepeat = "no-repeat";
-         
+
          magnifyingGlass.style.position = "absolute";
          magnifyingGlass.style.top = (-magnifierSize) + "px";
          magnifyingGlass.style.left = (-magnifierSize) + "px";
@@ -76,7 +76,7 @@ registerNSMethod(self, "apply", function(properties) {
    };
    script.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
    document.getElementsByTagName("head")[0].appendChild(script);
-   
+
    return true;
 });
 
