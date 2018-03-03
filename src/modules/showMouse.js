b92eecb9ee46dad8e39dd9102ada824e278c4c88
Tristram Newman <tristram.newman@gmail.com>
2018-03-03 14:58:48 +0000

    Tidied the modules a bit

--- src/modules/showMouse.js
+++ src/modules/showMouse.js
@@ -1,26 +1,11 @@
 registerNamespace("uk.org.adaptive.showMouse");
 
 self.isActive = false;
-self.mouseX = undefined;
-self.mouseY = undefined;
-self.ctrlDown = false;
-self.performingAnimation = false;
 
-self.onMouseMove = function(e) {
-   self.mouseX = e.pageX;
-   self.mouseY = e.pageY;
-}
-
-self.onKeyDown = function(e) {
-   if (e.keyCode === 16 && !self.ctrlDown) {
-      self.showMouse();
-      self.ctrlDown = true;
-   }
-}
-
-self.onKeyUp = function(e) {
-   if (e.keyCode === 16) self.ctrlDown = false;
-}
+var mouseX = undefined;
+var mouseY = undefined;
+var ctrlDown = false;
+var performingAnimation = false;
 
 registerNSMethod(self, "apply", function() {
    if (self.isActive) self.remove();
@@ -38,9 +23,25 @@ registerNSMethod(self, "remove", function() {
    window.removeEventListener("mousemove", self.onMouseMove);
 });
 
-registerNSMethod(self, "showMouse", function() {
-   if (typeof self.mouseX == "undefined" || typeof self.mouseY == "undefined" || self.performingAnimation) return;
-   console.log("Showing mouse at X: " + self.mouseX + ", Y: " + self.mouseY);
+self.onMouseMove = function(e) {
+   mouseX = e.pageX;
+   mouseY = e.pageY;
+}
+
+self.onKeyDown = function(e) {
+   if (e.keyCode === 16 && !ctrlDown) {
+      showMouse();
+      ctrlDown = true;
+   }
+}
+
+self.onKeyUp = function(e) {
+   if (e.keyCode === 16) ctrlDown = false;
+}
+
+const showMouse = function() {
+   if (typeof mouseX == "undefined" || typeof mouseY == "undefined" || performingAnimation) return;
+   console.log("Showing mouse at X: " + mouseX + ", Y: " + mouseY);
    
    var startSize = 400;
    var borderWidth = 8;
@@ -49,10 +50,10 @@ registerNSMethod(self, "showMouse", function() {
    var circle = document.createElement("div");
    circle.style.border = borderWidth + "px solid #000";
    circle.style.borderRadius = "50%";
-   circle.style.zIndex = "10000";     // TODO: this is a *really* bad way of doing it
+   circle.style.zIndex = "999999999";
    circle.style.position = "absolute";
-   circle.style.top = (self.mouseY - startSize/2) + "px";
-   circle.style.left = (self.mouseX - startSize/2) + "px";
+   circle.style.top = (mouseY - startSize/2) + "px";
+   circle.style.left = (mouseX - startSize/2) + "px";
    circle.style.width = startSize + "px";
    circle.style.height = startSize + "px";
    document.body.appendChild(circle);
@@ -60,16 +61,16 @@ registerNSMethod(self, "showMouse", function() {
    // Animation
    var size = startSize;
    var id = setInterval(frame, delta);
-   self.performingAnimation = true;
+   performingAnimation = true;
    
    function frame() {
       if (size <= 0) {
          clearInterval(id);
          circle.parentNode.removeChild(circle);
-         self.performingAnimation = false;
+         performingAnimation = false;
       } else {
-         circle.style.top = (self.mouseY - size/2) + "px";
-         circle.style.left = (self.mouseX - size/2) + "px";
+         circle.style.top = (mouseY - size/2) + "px";
+         circle.style.left = (mouseX - size/2) + "px";
          circle.style.width = size + "px";
          circle.style.height = size + "px";
          
@@ -81,4 +82,4 @@ registerNSMethod(self, "showMouse", function() {
          size -= 4;
       }
    }
-});
+};
