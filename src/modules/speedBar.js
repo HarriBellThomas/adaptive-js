3797e1b65148c1201f612bc0ed866e458910985b
louis <louisfharris@gmail.com>
2018-03-03 17:36:33 +0000

    Speed bar position fix.

--- src/modules/speedBar.js
+++ src/modules/speedBar.js
@@ -44,8 +44,8 @@ registerNSMethod(self, "apply",(
 
         area.className = "slider";
         area.style.position = "absolute";
-        (window.getComputedStyle(a).top) ? area.style.top = "0px" : area.style.top = a.style.top;
-        (window.getComputedStyle(a).top) ? area.style.left = "0px" : area.style.left = a.style.left;
+        (window.getComputedStyle(a).top == "auto") ? area.style.top = "0px" : area.style.top = a.style.top;
+        (window.getComputedStyle(a).top == "auto") ? area.style.left = "0px" : area.style.left = a.style.left;
         area.style.zIndex = 9999;
 
         slider.type = "range";
