20942582414f90d9d48ce3441d43648c48c3ddc5
Harri Bell-Thomas <harribt@live.co.uk>
2018-03-04 16:35:34 +0000

    Removed debug from compiled library

--- src/fresh/compileAll.php
+++ src/fresh/compileAll.php
@@ -2,7 +2,7 @@
   require("compileScript.php");
   header("Access-Control-Allow-Origin: *");
 
-  $modules = ["onDOMChange", "adaptiveBase", "adaptiveTools", "colourTools", "linkHighlighter", "visionTools", "showMouse", "typeWarning", "passwordReveal", "magnifier", "videoTools","darkMode", "imageColourShifter", "colorManipulations", "motorFeatures", "paragraphReader", "speedBar", "init", "debug"];
+  $modules = ["onDOMChange", "adaptiveBase", "adaptiveTools", "colourTools", "linkHighlighter", "visionTools", "showMouse", "typeWarning", "passwordReveal", "magnifier", "videoTools","darkMode", "imageColourShifter", "colorManipulations", "motorFeatures", "paragraphReader", "speedBar", "init"];
   $customScope = "NO";
   $selfURL = "https://js.adaptive.org.uk/adaptive.js";
 
