7bf6e1d9957e06d926e702f12c137376f86cd72e
louis <louisfharris@gmail.com>
2018-03-05 07:23:40 +0000

    Reformatted code for my modules.

--- src/modules/darkMode.js
+++ src/modules/darkMode.js
@@ -7,82 +7,82 @@ self.isActive = false;
 
 /* This will apply the dark mode function */
 
-registerNSMethod(self, "apply",(
-  function(){
+registerNSMethod(self, "apply", (
+    function () {
 
-    if (self.isActive)
-      self.remove();
+        if (self.isActive)
+            self.remove();
 
-    self.isActive = true;
+        self.isActive = true;
 
-    document.body.cacheCSSProperties(["color", "background-color"]);
-    document.body.style.backgroundColor = "rgb(25,25,25)";
-    document.body.style.color = "white";
+        document.body.cacheCSSProperties(["color", "background-color"]);
+        document.body.style.backgroundColor = "rgb(25,25,25)";
+        document.body.style.color = "white";
 
-    relevantTargets().where(a=> a instanceof HTMLElement).do(
-      a => {
-        if (!self.isActive) return;
-        /* Ensure non-destructiveness by caching CSS */
-          a.cacheCSSProperties(["color", "background-color"]);
-          alpha = rgbaValue(extractColour(a, "backgroundColor")).a;
-          a.style.color = "white";
-          a.style.backgroundColor = "rgba(25,25,25,"+alpha+")";
-      }
-    );
+        relevantTargets().where(a => a instanceof HTMLElement).do(
+            a => {
+                if (!self.isActive) return;
+                /* Ensure non-destructiveness by caching CSS */
+                a.cacheCSSProperties(["color", "background-color"]);
+                alpha = rgbaValue(extractColour(a, "backgroundColor")).a;
+                a.style.color = "white";
+                a.style.backgroundColor = "rgba(25,25,25," + alpha + ")";
+            }
+        );
 
-    forall(LINKS).where(a=> a instanceof HTMLElement).do(
-      a => {
-        if (!self.isActive) return;
-            a.cacheCSSProperties(["color"]);
-        a.style.color = "lightblue";
-      }
-    );
-  }
+        forall(LINKS).where(a => a instanceof HTMLElement).do(
+            a => {
+                if (!self.isActive) return;
+                a.cacheCSSProperties(["color"]);
+                a.style.color = "lightblue";
+            }
+        );
+    }
 ));
 
 
 /* Removes the dark mode function */
 
-registerNSMethod(self, "remove",(
-  function(){
-    self.isActive = false;
-    forall().where(a=> a instanceof HTMLElement).do(a=> {
-      a.resetCSS();
-    });
-    return true;
-  }
+registerNSMethod(self, "remove", (
+    function () {
+        self.isActive = false;
+        forall().where(a => a instanceof HTMLElement).do(a => {
+            a.resetCSS();
+        });
+        return true;
+    }
 ));
 
 
 /* This discards elements children to elements with background images */
-const relevantTargets = function(typ){
-  var output = [];
-  var queue=[document.body];
-  var n;
+const relevantTargets = function (typ) {
+    var output = [];
+    var queue = [document.body];
+    var n;
 
-  while(queue.length>0) {
-    n = queue.shift();
-    if (!n.children) {
-      continue;
+    while (queue.length > 0) {
+        n = queue.shift();
+        if (!n.children) {
+            continue;
+        }
+        if (n.className.toString().indexOf("logo") > -1) {
+            for (var i = 0; i < n.children.length; i++) {
+                n.children[i].className += "logo";
+            }
+        }
+        for (var i = 0; i < n.children.length; i++) {
+            img = window.getComputedStyle(n.children[i], null).backgroundImage;
+            var className = n.children[i].className.toString();
+            if (img.valueOf() == "none" || className.indexOf("logo") > -1) {
+                queue.push(n.children[i]);
+                if (typ == undefined || n.children[i].nodeName == typ.toString()) output.push(n.children[i]);
+            } else {
+                n.children[i].cacheCSSProperties(["background-image"]);
+                n.children[i].style.backgroundImage = "none";
+                queue.push(n.children[i]);
+                if (typ == undefined || n.children[i].nodeName == typ.toString()) output.push(n.children[i]);
+            }
+        }
     }
-    if (n.className.toString().indexOf("logo")>-1) {
-      for (var i=0; i<n.children.length; i++) {
-        n.children[i].className+="logo";
-      }
-    }
-    for (var i = 0; i< n.children.length; i++) {
-      img = window.getComputedStyle(n.children[i], null).backgroundImage;
-      var className = n.children[i].className.toString();
-      if (img.valueOf() == "none" || className.indexOf("logo") > -1) {
-        queue.push(n.children[i]);
-        if (typ == undefined || n.children[i].nodeName == typ.toString()) output.push(n.children[i]);
-      } else {
-        n.children[i].cacheCSSProperties(["background-image"]);
-        n.children[i].style.backgroundImage = "none";
-        queue.push(n.children[i]);
-        if (typ == undefined || n.children[i].nodeName == typ.toString()) output.push(n.children[i]);
-      }
-    }
-  }
-  return new Operable(output.reverse());
+    return new Operable(output.reverse());
 };
--- src/modules/speedBar.js
+++ src/modules/speedBar.js
@@ -8,77 +8,80 @@ self.isActive = false;
 
 /* Applies the module to add speed bar to all video elements */
 
-registerNSMethod(self, "apply",(
-  function(properties) {
+registerNSMethod(self, "apply", (
+    function (properties) {
 
-    if (!verifyArgs(properties, [["default", NUMTYPE]]))
-      return false;
+        if (!verifyArgs(properties, [["default", NUMTYPE]]))
+            return false;
 
-    initVal = properties["default"];
+        initVal = properties["default"];
 
-    if (self.isActive)
-      self.remove();
+        if (self.isActive)
+            self.remove();
 
-    self.isActive = true;
+        self.isActive = true;
 
-    forall(VIDEOS).do(
-      function (a) {
-        area = document.createElement("DIV");
-        slider = document.createElement("INPUT");
-        output = document.createElement("DIV");
+        forall(VIDEOS).do(
+            function (a) {
+                area = document.createElement("DIV");
+                slider = document.createElement("INPUT");
+                output = document.createElement("DIV");
 
-        area.className = "slider";
-        area.style.position = "absolute";
-        area.style.top = a.offsetTop+"px";
-        area.style.left = a.offsetLeft+"px";
-        area.style.zIndex = "9999";
+                area.className = "slider";
+                area.style.position = "absolute";
+                area.style.top = a.offsetTop + "px";
+                area.style.left = a.offsetLeft + "px";
+                area.style.zIndex = "9999";
 
-        slider.type = "range";
-        slider.min = "0.2";
-        slider.max = "2";
-        slider.step = "0.1";
-        slider.value = initVal;
-        area.style.opacity = "0.25";
+                slider.type = "range";
+                slider.min = "0.2";
+                slider.max = "2";
+                slider.step = "0.1";
+                slider.value = initVal;
+                area.style.opacity = "0.25";
 
-        output.innerHTML = initVal;
-        a.playbackRate = initVal;
-        output.style.display="inline";
-        output.style.color = "rgb(127,127,127)";
-        output.style.height = area.height;
+                output.innerHTML = initVal;
+                a.playbackRate = initVal;
+                output.style.display = "inline";
+                output.style.color = "rgb(127,127,127)";
+                output.style.height = area.height;
 
-        area.appendChild(slider);
-        area.appendChild(output);
-        a.parentNode.insertBefore(area, a.nextSibling);
-        fadeTimer = window.setTimeout(function(){},0);
+                area.appendChild(slider);
+                area.appendChild(output);
+                a.parentNode.insertBefore(area, a.nextSibling);
+                fadeTimer = window.setTimeout(function () {
+                }, 0);
 
 
-        // Update the current slider value (each time you drag the slider handle)
+                // Update the current slider value (each time you drag the slider handle)
 
-        slider.oninput = function() {
-          window.clearTimeout(fadeTimer);
-          a.nextSibling.style.opacity = "1";
-          a.nextSibling.children[1].innerHTML = Math.round(10*this.value)/10;
-          a.nextSibling.children[1].style.color = "yellow";
-          a.play();
-          a.playbackRate = this.value;
-          fadeTimer = window.setTimeout(function(){a.nextSibling.style.opacity="0.25";a.nextSibling.children[1].style.color = "rgb(127,127,127)"
-          }, 2000);
-        };
-      })
-  }
+                slider.oninput = function () {
+                    window.clearTimeout(fadeTimer);
+                    a.nextSibling.style.opacity = "1";
+                    a.nextSibling.children[1].innerHTML = Math.round(10 * this.value) / 10;
+                    a.nextSibling.children[1].style.color = "yellow";
+                    a.play();
+                    a.playbackRate = this.value;
+                    fadeTimer = window.setTimeout(function () {
+                        a.nextSibling.style.opacity = "0.25";
+                        a.nextSibling.children[1].style.color = "rgb(127,127,127)"
+                    }, 2000);
+                };
+            })
+    }
 ));
 
 /* Now we define the method 'remove' which removes the effect
     from the page                                             */
 
-registerNSMethod(self, "remove",(
-  function(){
-    if(self.isActive = false) return true;
-    self.isActive = false;
-    forall(VIDEOS).do(function(a) {
-      b = a.nextSibling;
-      b.parentNode.removeChild(b);
-    });
-    return true;
-  }
+registerNSMethod(self, "remove", (
+    function () {
+        if (self.isActive = false) return true;
+        self.isActive = false;
+        forall(VIDEOS).do(function (a) {
+            b = a.nextSibling;
+            b.parentNode.removeChild(b);
+        });
+        return true;
+    }
 ));
